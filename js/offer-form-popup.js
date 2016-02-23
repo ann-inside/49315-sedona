94d79a0ca49e7829c0bd1cc344361265119afe01
ann-inside <nuro4-ka@mail.ru>
2016-02-23 19:14:00 +0600

    JS анимация

--- js/offer-form-popup.js
+++ js/offer-form-popup.js
@@ -1,13 +1,13 @@
-var btn_form = document.querySelector(".offer-form-btn-big")
-var popup = document.querySelector(".offer-form-popup")
-var close = document.querySelector(".offer-form-btn-big")
+var btn_form = document.querySelector(".offer-form-btn-big");
+var popup = document.querySelector(".offer-form-popup");
+var close = document.querySelector(".offer-form-close");
 
 btn_form.addEventListener("click", function(event){
-    event.PreventDefault();
+    event.preventDefault();
     popup.classList.add("offer-form-popup-show");
 });
 
 close.addEventListener("click", function(event){
-    event.PreventDefault();
+    event.preventDefault();
     popup.classList.remove("offer-form-popup-show");
 }) 
